c3b09371527a12adbaebdebd74b7c71cef310443
LionKing <trantrunghieu@Tran-Hieus-iMac.local>
2014-04-21 09:55:22 +0700

    commit

---
+++ Boomberman/Classes/AppDelegate.h
@@ -0,0 +1,13 @@
+//
+//  AppDelegate.h
+//  Boomberman
+//
+//  Created by LionKing on 4/21/14.
+//  Copyright Anonymous 2014. All rights reserved.
+//
+// -----------------------------------------------------------------------
+
+#import "cocos2d.h"
+
+@interface AppDelegate : CCAppDelegate
+@end
---
+++ Boomberman/Supporting Files/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Boomberman
+//
+//  Created by LionKing on 4/21/14.
+//  Copyright Anonymous 2014. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    @autoreleasepool {
+        int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+        return retVal;
+    }
+}
